722f8c6e32f2557455eee568201b10afd0b2b13b
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2022-09-29 19:25:21 +0200

    add l-5

---
+++ story-fish/.storybook/preview.js
@@ -0,0 +1,25 @@
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => (
+    <OriginalNextImage
+      {...props}
+      unoptimized
+      loading="eager"
+      decoding="async"
+    />
+  ),
+});
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
