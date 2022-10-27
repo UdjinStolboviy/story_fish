9305dbf86750252830543d38a697962f0c829e84
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2022-10-27 13:47:39 +0200

    add fix

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "my-salt"),
+  },
+  auth: {
+    secret: env("ADMIN_JWT_SECRET", "Pcqj65jg9uqQ7lznz3Gwrg=="),
   },
 });
